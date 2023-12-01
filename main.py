37508c49b36b126c32f2aca4ce6ca0d8fefa3125
Kostychenko Albina <albina.kostyuchenko03@gmail.com>
2023-12-01 14:44:47 +0200

    first version fixed

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from sklearn.neighbors import NearestNeighbors
 from fuzzywuzzy import process
 import chardet
 
-pd.set_option('display.max_colwidth', 150)
+pd.set_option('display.max_colwidth', 160)
 
 hotels = 'booking_hotel.csv'
 
@@ -24,17 +24,16 @@ df_hotels['Number of'].fillna(0, inplace=True)
 hotels_users = df_hotels.pivot(columns='Location', values='Rating').fillna(0)
 mat_hotels_users = csr_matrix(hotels_users.values)
 
-model_knn = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=10)
+model_knn = NearestNeighbors(metric='euclidean', algorithm='ball_tree', n_neighbors=10)
 model_knn.fit(mat_hotels_users)
 
 def hotel_recommender(hotel_name, data, model, n_recommendations):
     model.fit(data)
     idx = process.extractOne(hotel_name, df_hotels['Hotel Name'])[2]
     print('Hotel Selected: ', df_hotels['Hotel Name'][idx], 'Index: ', idx)
-    print('Searching for recommendations.....')
+    print('Searching for recommendations...')
     distances, indices = model.kneighbors(data[idx], n_neighbors=n_recommendations)
     for i in indices:
         print(df_hotels['Hotel Name'][i].where(i != idx))
 
-hotel_recommender('FuramaXclusive Sathorn', mat_hotels_users, model_knn, 10)
-
+hotel_recommender('Bo Phut Resort and Spa - SHA Plus', mat_hotels_users, model_knn,10)
